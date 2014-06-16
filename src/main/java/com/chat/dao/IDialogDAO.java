11c225bc2c419046f870db1b8e5f26f0b0a7620a
kukharroma <kukharroma@gmail.com>
2014-06-16 19:52:48 +0300

    delete some unnecessary fiels

--- src/main/java/com/chat/dao/IDialogDAO.java
+++
@@ -1,9 +0,0 @@
-package com.chat.dao;
-
-import com.chat.model.Dialog;
-import com.google.code.morphia.dao.DAO;
-import org.bson.types.ObjectId;
-
-
-public interface IDialogDAO extends DAO<Dialog, ObjectId> {
-}
--- src/main/java/com/chat/services/IDialogService.java
+++
@@ -1,9 +0,0 @@
-package com.chat.services;
-
-
-import com.chat.model.Dialog;
-
-public interface IDialogService {
-
-    public void save(Dialog dialog);
-}
