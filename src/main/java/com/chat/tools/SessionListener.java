ae5912916defc532f9e4bf029d063a9f186a4ce6
kukharroma <kukharroma@gmail.com>
2014-07-31 11:24:53 +0300

    tools documented

--- src/main/java/com/chat/tools/SessionListener.java
+++ src/main/java/com/chat/tools/SessionListener.java
@@ -1,5 +1,6 @@
 package com.chat.tools;
 
+import com.chat.controllers.BaseController;
 import com.chat.dao.IDBConfiguration;
 import com.chat.dao.IUserDAO;
 import com.chat.dao.impl.UserDAO;
@@ -14,18 +15,29 @@ import javax.servlet.http.HttpSessionListener;
 import java.net.UnknownHostException;
 
 /**
- * 
+ * Represents method  which will be notified when user's session
+ * created or destroyed
  */
-public class SessionListener implements HttpSessionListener, IDBConfiguration {
+public class SessionListener extends BaseController implements HttpSessionListener, IDBConfiguration {
 
     private IUserDAO userDAO;
 
     private static final Logger log = Logger.getLogger(SessionListener.class);
 
+    /**
+     * Notification that a session was created.
+     *
+     * @param sessionEvent the notification event
+     */
     @Override
     public void sessionCreated(HttpSessionEvent sessionEvent) {
     }
 
+    /**
+     * Notification that a session is about to be invalidated.
+     *
+     * @param sessionEvent the notification event
+     */
     @Override
     public void sessionDestroyed(HttpSessionEvent sessionEvent) {
         if (getLoggedPerson() != null) {
@@ -43,10 +55,10 @@ public class SessionListener implements HttpSessionListener, IDBConfiguration {
     }
 
     /**
-     * This method returns the user which is logged
+     * Returns the user which is logged
      * @return instance of user
      */
-    private User getLoggedPerson() {
+    protected User getLoggedPerson() {
         if (SecurityContextHolder.getContext().getAuthentication() == null
                 || SecurityContextHolder.getContext().getAuthentication()
                 .getPrincipal().equals("anonymousUser")) {
--- src/main/java/com/chat/tools/TimeFormater.java
+++ src/main/java/com/chat/tools/TimeFormater.java
@@ -1,17 +1,24 @@
 package com.chat.tools;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
- *
+ *  Represent methods for work with date.
+ *  Change or formatting date.
  */
 public class TimeFormater {
 
     private static final SimpleDateFormat DATE_FORMATER = new SimpleDateFormat();
     private static final String TIME_WITH_SECONDS_PATTERN = "HH:mm:ss dd.MM.yyyy";
 
+    /**
+     * Formats the date object using SimpleDateFormat
+     * and some pattern.
+     *
+     * @param date you want to format
+     * @return formatted date
+     */
     public static String formatDateWithSeconds(Date date) {
         DATE_FORMATER.applyPattern(TIME_WITH_SECONDS_PATTERN);
         return DATE_FORMATER.format(date);
