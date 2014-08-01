c873144d4e39bdfdaaca6f6c1582605120a902dd
kukharroma <kukharroma@gmail.com>
2014-08-01 23:57:58 +0300

    documented RegistrationServiceTest

--- src/test/java/com/chat/RegistrationServiceTest.java
+++ src/test/java/com/chat/RegistrationServiceTest.java
@@ -15,6 +15,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import javax.annotation.Resource;
 import java.util.Map;
 
+/**
+ * Represents methods which execute tests
+ * on class RegistrationService
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:/all-spring-config.xml")
 public class RegistrationServiceTest extends Assert {
@@ -25,12 +29,19 @@ public class RegistrationServiceTest extends Assert {
     @Resource(name = "userService")
     private UserService userService;
 
+    /**
+     * Clears database from all registered users
+     * before and after each test
+     */
     @Before
     @After
     public void clearDB() {
         userService.deleteAllUsers();
     }
 
+    /**
+     * Tests registration a user who is valid
+     */
     @Test
     public void testRegistrationUser() {
         User user = createUser("name", "password", false);
@@ -40,6 +51,10 @@ public class RegistrationServiceTest extends Assert {
         assertTrue(equalsUsers(user, testUser));
     }
 
+    /**
+     * Tests registration a user who is not valid.
+     * User has empty mame.
+     */
     @Test
     public void testRegistrationUserEmptyName() {
         User user = createUser("", "password", false);
@@ -49,6 +64,10 @@ public class RegistrationServiceTest extends Assert {
         assertTrue(map.containsKey("nameFailed"));
     }
 
+    /**
+     * Tests registration a user who is not valid.
+     * User has empty password.
+     */
     @Test
     public void testRegistrationUserEmptyPassword() {
         User user = createUser("name", "", false);
@@ -58,6 +77,10 @@ public class RegistrationServiceTest extends Assert {
         assertTrue(map.containsKey("passwordFailed"));
     }
 
+    /**
+     * Tests registration a user who is not valid.
+     * User has empty password and name.
+     */
     @Test
     public void testRegistrationUserEmptyPasswordAndName() {
         User user = createUser("", "", false);
@@ -68,6 +91,10 @@ public class RegistrationServiceTest extends Assert {
         assertTrue(map.containsKey("nameFailed"));
     }
 
+    /**
+     * Tests registration a user into database
+     * User is valid.
+     */
     @Test
     public void testRegisterUser() {
         User user = createUser("name", "password", false);
@@ -76,6 +103,10 @@ public class RegistrationServiceTest extends Assert {
         assertTrue(equalsUsers(user, testUser));
     }
 
+    /**
+     * Tests registration a user into database
+     * when user has empty name
+     */
     @Test
     public void testRegisterUserEmptyName() {
         User user = createUser("", "password", false);
@@ -84,6 +115,10 @@ public class RegistrationServiceTest extends Assert {
         assertTrue(equalsUsers(user, testUser));
     }
 
+    /**
+     * Tests registration a user into database
+     * when user has empty password
+     */
     @Test
     public void testRegisterUserEmptyPassword() {
         User user = createUser("name", "", false);
@@ -92,6 +127,10 @@ public class RegistrationServiceTest extends Assert {
         assertTrue(equalsUsers(user, testUser));
     }
 
+    /**
+     * Tests registration a user into database
+     * when user has empty password and name
+     */
     @Test
     public void testRegisterUserEmptyPasswordAndName() {
         User user = createUser("", "", false);
@@ -100,6 +139,9 @@ public class RegistrationServiceTest extends Assert {
         assertTrue(equalsUsers(user, testUser));
     }
 
+    /**
+     * Tests setting authority for user
+     */
     @Test
     public void testSetAuthority() {
         User user = createUser("name", "password", false);
@@ -107,12 +149,22 @@ public class RegistrationServiceTest extends Assert {
         assertNotNull(user.getAuthorities());
     }
 
+    /**
+     * Tests setting authority if user is null
+     */
     @Test(expected = NullPointerException.class)
     public void testSetAuthorityForNull() {
         registrationService.setAuthority(null);
     }
 
-
+    /**
+     * Equals two instance of User. If objects are equal
+     * returns true else returns false.
+     *
+     * @param firstUser  user you want to compare
+     * @param secondUser user with whom you want to compare
+     * @return If objects are equal returns true else returns false.
+     */
     private boolean equalsUsers(User firstUser, User secondUser) {
         boolean result = true;
         result = result && firstUser.getId().equals(secondUser.getId());
@@ -122,6 +174,14 @@ public class RegistrationServiceTest extends Assert {
         return result;
     }
 
+    /**
+     * Creates an instance of User
+     *
+     * @param name     name of user
+     * @param password user's password
+     * @param online   true if user is online, false if user is offline.
+     * @return created instance of User
+     */
     private User createUser(String name, String password, Boolean online) {
         User user = new User();
         user.setId(new ObjectId());
